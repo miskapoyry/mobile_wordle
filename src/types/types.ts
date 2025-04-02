11a5f57061bc0ef2c559c20e01a20ac25a20cf42
Miska Poyry <bhd692@myy.haaga-helia.fi>
2025-04-02 15:14:42 +0300

    Lisaa NoAuthNavin tyypit

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,9 +1,14 @@
 export interface HeaderProps {
     title: string;
     description: string;
-}
+};
 
 export type FadeAnimationProps = {
     children: React.ReactNode;
     duration: number;
-}
+};
+
+export type NoAuthParams = {
+    Login: undefined;
+    Register: undefined;
+};
