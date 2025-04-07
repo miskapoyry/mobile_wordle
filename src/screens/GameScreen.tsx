438a6b23bace8e764469f53feb180aba408f7883
Miska Poyry <bhd692@myy.haaga-helia.fi>
2025-04-07 18:05:35 +0300

    Lisaa yksinkertainen game screen

---
+++ src/screens/GameScreen.tsx
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from "react";
+import { View, ActivityIndicator, StyleSheet } from "react-native";
+import { useRoute } from "@react-navigation/native";
+import GameBoard from "../components/GameBoard";
+import { fetchRandomWord, validateRandomWord } from "../utils/wordService";
+import Loading from "../components/Loading";
+
+export default function GameScreen() {
+  const route = useRoute();
+  const { wordLength } = route.params as { wordLength: number };
+  const [targetWord, setTargetWord] = useState<string | null>(null);
+
+  useEffect(() => {
+    const loadValidWord = async () => {
+      let validWord: string | null = null;
+  
+      while (!validWord) {
+        const word = await fetchRandomWord(wordLength);
+        const isValid = await validateRandomWord(word);
+  
+        if (isValid) {
+            console.log(word)
+          validWord = word;
+        }
+      }
+  
+      setTargetWord(validWord);
+    };
+  
+    loadValidWord();
+  }, [wordLength]);
+
+  if (!targetWord) {
+    return <Loading />;
+  }
+
+  return <GameBoard targetWord={targetWord} maxGuesses={6} />;
+}
