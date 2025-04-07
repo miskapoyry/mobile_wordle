fdbec115b26178fd92f26d2114ec8fa552519dde
Miska Poyry <bhd692@myy.haaga-helia.fi>
2025-04-07 18:06:21 +0300

    Lisaa gameboard komponentti

---
+++ src/components/GameBoard.tsx
@@ -0,0 +1,71 @@
+import React, { useState } from "react";
+import { View, TextInput, Button, SafeAreaView, Alert } from "react-native";
+import { validateRandomWord } from "../utils/wordService";
+import { Text } from "react-native-paper";
+
+type Props = {
+    targetWord: string;
+    maxGuesses: number;
+};
+
+export default function GameBoard({ targetWord, maxGuesses }: Props) {
+    const [guess, setGuess] = useState("");
+    const [guesses, setGuesses] = useState<string[]>([]);
+    const [status, setStatus] = useState<"playing" | "won" | "lost">("playing");
+
+    const handleGuess = async () => {
+
+        const validatedGuess = await validateRandomWord(guess.toLowerCase());
+        if (!validatedGuess) {
+            Alert.alert("Invalid word. Please try again.");
+            return;
+        }
+
+        if (guesses.includes(guess.toLowerCase())) {
+            Alert.alert("You already guessed that word!");
+            return;
+        }
+
+        if (guess.length !== targetWord.length) {
+            Alert.alert(`Guess must be ${targetWord.length} letters long.`);
+            return;
+        }
+
+        const newGuesses = guesses.concat(guess.toLowerCase());
+        setGuesses(newGuesses);
+        setGuess("");
+
+
+        if (guess.toLowerCase() === targetWord) {
+            setStatus("won");
+        } else if (newGuesses.length >= maxGuesses) {
+            setStatus("lost");
+        }
+    };
+
+    return (
+        <SafeAreaView>
+            <Text variant="titleLarge" style={{ marginBottom: 10, textAlign: "center" }}>Target word length: {targetWord.length}</Text>
+
+            {guesses.map((g, i) => (
+                <Text key={i} variant="titleMedium" style={{ marginBottom: 10, textAlign: "center" }}>{g}</Text>
+            ))}
+
+            {status === "playing" && (
+                <>
+                    <TextInput
+                        value={guess}
+                        onChangeText={setGuess}
+                        placeholder="Your guess"
+                        style={{ borderBottomWidth: 1, marginVertical: 12 }}
+                    />
+                    <Button title="Guess" onPress={handleGuess} />
+                </>
+            )}
+
+            {status === "won" && <Text style={{ marginTop: 10, color: "green" }}>Correct! You won!</Text>}
+            {status === "lost" && <Text style={{ marginTop: 10, color: "red" }}>Game over. Word was: {targetWord}</Text>}
+
+        </SafeAreaView>
+    );
+}
