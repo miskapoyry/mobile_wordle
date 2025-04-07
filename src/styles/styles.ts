d35f4c2fe71d8ce76e349f88d51026c3bc2ec0e7
Miska Poyry <bhd692@myy.haaga-helia.fi>
2025-04-07 18:15:11 +0300

    formatoi tyylit ja lisaa uusia

--- src/styles/styles.ts
+++ src/styles/styles.ts
@@ -1,34 +1,39 @@
 import { StyleSheet } from "react-native"
 
 export const styles = StyleSheet.create({
-    background: {
-        flex: 1,
-        flexGrow: 1
-    },
-    title: {
-        textAlign: "center",
-        padding: 5,
-        fontSize: 40,
-        fontWeight: "bold",
-    },
-    description: {
-        textAlign: "center",
-        fontSize: 16
-    },
-    intro: {
-        textAlign: "center"
-    },
-    container: {
-        flex: 1,
-        padding: 16,
-        justifyContent: 'center',
-      },
-      buttonContainer: {
-        marginTop: 20,
-        gap: 10,
-        alignItems: 'center',
-      },
-      button: {
-        width: 250,
-      },
+  loading: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  background: {
+    flex: 1,
+    flexGrow: 1
+  },
+  title: {
+    textAlign: "center",
+    padding: 5,
+    fontSize: 40,
+    fontWeight: "bold",
+  },
+  description: {
+    textAlign: "center",
+    fontSize: 16
+  },
+  intro: {
+    textAlign: "center"
+  },
+  container: {
+    flex: 1,
+    padding: 16,
+    justifyContent: 'center',
+  },
+  buttonContainer: {
+    marginTop: 20,
+    gap: 10,
+    alignItems: 'center',
+  },
+  button: {
+    width: 250,
+  }
 })
