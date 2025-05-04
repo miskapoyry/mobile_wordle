306f78d96f9b3c8307c2405b17a78f2dec85a4a8
Miska Poyry <bhd692@myy.haaga-helia.fi>
2025-05-04 15:31:04 +0300

    Tee uusi service, jossa pelaajan rank voidaan hakea

---
+++ src/utils/rankService.ts
@@ -0,0 +1,33 @@
+export const ranks = [
+    { name: "NOOB", minPoints: -Infinity, maxPoints: -1, animation: require("../assets/noobAnimation.json"), },
+    { name: "BEGINNER", minPoints: 0, maxPoints: 999, animation: require("../assets/beginnerAnimation.json"), },
+    { name: "ADVANCED", minPoints: 1000, maxPoints: 2499, animation: require("../assets/advancedAnimation.json"), },
+    { name: "PROFESSIONAL", minPoints: 2500, maxPoints: 4999, animation: require("../assets/professionalAnimation.json"), },
+    { name: "GODLIKE", minPoints: 5000, maxPoints: 10000, animation: require("../assets/godAnimation.json"), },
+]
+
+export const getPlayerRank = (points: number) => {
+    const playerRank = ranks.find(rank => points >= rank.minPoints && points <= rank.maxPoints);
+
+    if (!playerRank) {
+        return {
+            name: "No Rank",
+            progress: 0,
+            rankMinPoints: 0,
+            rankMaxPoints: 0,
+            animation: null,
+        }
+    }
+
+    const rankPoints = playerRank.maxPoints - playerRank.minPoints;
+    const rankProgress = points - playerRank.minPoints;
+    const progress = rankProgress / rankPoints;
+
+    return {
+        name: playerRank.name,
+        progress: progress,
+        rankMinPoints: playerRank.minPoints,
+        rankMaxPoints: playerRank.maxPoints,
+        animation: playerRank.animation,
+    }
+};  
